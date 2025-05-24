ed30d044b16ed9ff5e3d307897a22369f19f34be
Aytan <aytangurbanova05@gmail.com>
2025-05-24 13:16:20 +0300

    css updates

--- public/auth.php
+++ public/auth.php
@@ -8,12 +8,20 @@ if (isset($_SESSION['id'])) {
     exit;
 }
 ?>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
 
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="assets/css/auth.css">
 <script src="assets/js/auth.js"></script>
 <script src="assets/js/logout.js"></script>
 
 <body>
+
 <div class="container">
     <div id="main-container" class="main-container" style="<?php echo isUserLoggedIn() ? 'display:block' : 'display:none' ?>">
         <button id="logout-button" class="btn btn-danger" onclick="logoutUser()">Logout</button>
@@ -41,7 +49,7 @@ if (isset($_SESSION['id'])) {
             </form>
         </div>
         <div id="register-section" style="display:none;">
-            <h2 class="text-center mb-4">Register for Game Jam</h2>
+            <h2 class="text-center mb-4">Register for GameJam</h2>
             <form id="register-form">
                 <div class="mb-3">
                     <input type="text" class="form-control" id="register-username" placeholder="Username" required>
--- public/dashboard.php
+++ public/dashboard.php
@@ -10,8 +10,15 @@ require_once '../includes/header.php';
 require_once '../includes/footer.php';
 ?>
 
-<link rel="stylesheet" href="assets/css/dashboard.css">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
 
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="assets/css/dashboard.css">
 <script src="assets/js/logout.js"></script>
 
 <body>
